1303fe509451252112ba88880f6c64a6af695a51
unknown <you@example.com>
2021-04-04 02:27:35 -0300

    aprendendo a instalar e utilizar pacotes

---
+++ NPM-demo/index.js
@@ -0,0 +1,7 @@
+//instalamos um modulo utilizando npm i <nome do modulo> , neste caso npm i underscore
+
+var _ = require('underscore');
+
+var result = _.contains([1, 2, 3], 2);
+console.log(result);
+
